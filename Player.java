b9fd41552f0af710509d6d036c7a4415642ad158
kpedroc <karl_kpc@hotmail.com>
2023-11-07 16:16:02 +0100

    Player class with constructor

--- Player.java
+++ Player.java
@@ -1,3 +1,19 @@
-public class Player {
+public class Player{
+   
+    String name;
+    int coin;
     
+    public Player(String name, int coin) {
+        this.name = name;
+        this.coin = coin;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getCoin() {
+        return coin;
+    }
+    public void addCoins(int value) {
+            coin =+ value;
+    }
 }
