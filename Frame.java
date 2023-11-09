efccec562ccfc430b27f1c7c7a6ab8e67df46ed9
kpedroc <karl_kpc@hotmail.com>
2023-11-09 10:39:59 +0100

    Playerpointslabel on the side to track money

--- Frame.java
+++ Frame.java
@@ -1,108 +1,136 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class Frame extends JFrame implements ActionListener{
-	JButton rollButton, skipButton;
-	JButton[] playerButton = new JButton[3];
-	JLabel label;
-	JPanel botPanel, midPanel;
-	Font myFont = new Font("myFont", Font.BOLD, 25);
-	ImageIcon logo;
 
-	Frame(){
+public class Frame extends JFrame implements ActionListener {
+    JButton rollButton, skipButton;
+    JButton[] playerButton = new JButton[3];
+    JLabel label, plabel;
+    JLabel[] pointsLabel;
+    JPanel botPanel, midPanel, rightPanel;
+    Font myFont = new Font("myFont", Font.BOLD, 23);
+    Font smallFont = new Font("smallFont", Font.BOLD, 15);
+    ImageIcon logo;
 
-		label = new JLabel("Choose the number of players");
-		label.setBounds(150, 150, 300, 200);
+    Frame() {
+        label = new JLabel("Choose the number of players");
         label.setFont(myFont);
         label.setBackground(Color.gray);
-		label.setVisible(true);
+        label.setVisible(true);
 
-		botPanel = new JPanel();
-		midPanel = new JPanel();
-		botPanel.setBackground(Color.darkGray);
-		midPanel.setBackground(Color.lightGray);
-		botPanel.setPreferredSize(new Dimension(100, 100));
-		midPanel.setPreferredSize(new Dimension(100, 100));
-		
-		rollButton = new JButton("Roll");
-		rollButton.setBounds(0, 0, 50, 25);
-		rollButton.addActionListener(this);
-		rollButton.setFocusable(false);
-		rollButton.setHorizontalTextPosition(JButton.CENTER);
-		rollButton.setVerticalTextPosition(JButton.CENTER);
-		rollButton.setFont(myFont);
-		rollButton.setBackground(Color.white);
-		rollButton.setVisible(false);
+        botPanel = new JPanel();
+        botPanel.setBackground(Color.darkGray);
+        botPanel.setPreferredSize(new Dimension(100, 100));
+        botPanel.setLayout(new FlowLayout());
 
-		skipButton = new JButton("Pass");
-		skipButton.setBounds(0, 0, 50, 25);
-		skipButton.addActionListener(this);
-		skipButton.setFocusable(false);
-		skipButton.setHorizontalTextPosition(JButton.CENTER);
-		skipButton.setVerticalTextPosition(JButton.CENTER);
-		skipButton.setFont(myFont);
-		skipButton.setBackground(Color.white);
-		skipButton.setVisible(false);
-		
-		for (var i=0; i<3; i++) {
-			playerButton[i] = new JButton(String.valueOf(i+2));
-			playerButton[i].addActionListener(this);
-			playerButton[i].setFont(myFont);
-			playerButton[i].setFocusable(false);
-			botPanel.add(playerButton[i]);
-		}
-		
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(null);
-		this.setSize(500,500);
-		this.setVisible(true);
+        midPanel = new JPanel();
+        midPanel.setBackground(Color.lightGray);
+        midPanel.setPreferredSize(new Dimension(100, 100));
+        midPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+        rightPanel = new JPanel();
+        rightPanel.setBackground(Color.orange);
+        rightPanel.setPreferredSize(new Dimension(125, 100));
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+
+        rollButton = new JButton("Roll");
+        rollButton.setBounds(0, 0, 50, 25);
+        rollButton.addActionListener(this);
+        rollButton.setFocusable(false);
+        rollButton.setHorizontalTextPosition(JButton.CENTER);
+        rollButton.setVerticalTextPosition(JButton.CENTER);
+        rollButton.setFont(myFont);
+        rollButton.setBackground(Color.white);
+        rollButton.setVisible(false);
+
+        skipButton = new JButton("Pass");
+        skipButton.setBounds(0, 0, 50, 25);
+        skipButton.addActionListener(this);
+        skipButton.setFocusable(false);
+        skipButton.setHorizontalTextPosition(JButton.CENTER);
+        skipButton.setVerticalTextPosition(JButton.CENTER);
+        skipButton.setFont(myFont);
+        skipButton.setBackground(Color.white);
+        skipButton.setVisible(false);
+
+        for (var i = 0; i < 3; i++) {
+            playerButton[i] = new JButton(String.valueOf(i + 2));
+            playerButton[i].addActionListener(this);
+            playerButton[i].setFont(myFont);
+            playerButton[i].setFocusable(false);
+            botPanel.add(playerButton[i]);
+        }
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setSize(500, 500);
+        this.setVisible(true);
         this.setForeground(Color.darkGray);
-		this.setLayout(new BorderLayout());
-		botPanel.add(rollButton);
-		botPanel.add(skipButton);
-		midPanel.add(label);
-		this.add(botPanel, BorderLayout.SOUTH);
-		this.add(midPanel, BorderLayout.CENTER);
-		
+        this.setLayout(new BorderLayout());
+        botPanel.add(rollButton);
+        botPanel.add(skipButton);
+        midPanel.add(label);
+        this.add(botPanel, BorderLayout.SOUTH);
+        this.add(midPanel, BorderLayout.CENTER);
+        this.add(rightPanel, BorderLayout.EAST);
 
-	}
-	
-	public void turnRoll(boolean isTrue) {
-		if(isTrue) {
-			skipButton.setVisible(false);
-			rollButton.setVisible(true);
-		}
-		else {
-			skipButton.setVisible(true);
-			rollButton.setVisible(false);
-		}
-	}
+    }
 
-	//Choosing number of players, and starts the game.
-	@Override
-	public void actionPerformed(ActionEvent click) {
-		for(int j=0; j<3; j++) {
-			if(click.getSource()==playerButton[j]) {
-				Player.totalPlayers(j+2);
-				Player.createPlayers();
-				for (var i=0; i<3; i++) {
-					playerButton[i].setVisible(false);
-				}
-				rollButton.setVisible(true);
-				label.setText(Player.getCurrentPlayer().getName()+" starts. Press the Roll rollButton");
-			}
-		}
-		if(click.getSource()==rollButton) {
-				turnRoll(false);
-				var rollResult = Die.dieRoll(); // Call the dieRoll method on the class itself
-				Player.getCurrentPlayer().addCoins(rollResult); //Change later to money from the game!!!!
-				label.setText(Player.getCurrentPlayer().getName()+" rolled: " + rollResult);
-			}
-		if(click.getSource()==skipButton) {
-			Player.switchPlayer();
-			label.setText(Player.getCurrentPlayer().getName()+" its your turn now, roll");
-			turnRoll(true);
-		}
-	}
+    public void turnRoll(boolean isTrue) {
+        if (isTrue) {
+            skipButton.setVisible(false);
+            rollButton.setVisible(true);
+        } else {
+            skipButton.setVisible(true);
+            rollButton.setVisible(false);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent click) {
+        for (int j = 0; j < 3; j++) {
+            if (click.getSource() == playerButton[j]) {
+                Player.totalPlayers(j + 2);
+                Player.createPlayers();
+                for (var i = 0; i < 3; i++) {
+                    playerButton[i].setVisible(false);
+                }
+                rollButton.setVisible(true);
+                label.setText(Player.getCurrentPlayer().getName() + " starts. Roll!");
+                getPlayerLabels();
+            }
+        }
+        if (click.getSource() == rollButton) {
+            turnRoll(false);
+            var rollResult = Die.dieRoll();
+            Player.getCurrentPlayer().addCoins(rollResult);
+            label.setText(Player.getCurrentPlayer().getName() + " rolled: " + rollResult);
+            getPlayerLabels();
+        }
+        if (click.getSource() == skipButton) {
+            Player.switchPlayer();
+            label.setText(Player.getCurrentPlayer().getName() + " it's your turn now, roll");
+            turnRoll(true);
+        }
+    }
+
+    public JLabel[] getPlayerLabels() {
+        // Clear existing labels
+        rightPanel.removeAll();
+
+        pointsLabel = new JLabel[Player.playerNumbers];
+        for (int i = 0; i < Player.playerNumbers; i++) {
+            pointsLabel[i] = new JLabel("P" + (i + 1) + ": " + Player.getPlayerNumb(i).getCoin() + "$");
+            pointsLabel[i].setFont(myFont);
+            pointsLabel[i].setForeground(Color.BLACK);
+            pointsLabel[i].setVisible(true);
+            rightPanel.add(pointsLabel[i]);
+        }
+
+        // Refresh the GUI
+        rightPanel.revalidate();
+        rightPanel.repaint();
+
+        return pointsLabel;
+    }
 }
-    
--- Player.java
+++ Player.java
@@ -1,7 +1,7 @@
 public class Player {
     String name;
     int coin;
-    static int playernumbers = 0;
+    static int playerNumbers = 0;
     private static Player[] players;
     private static int playerIndex = 0;
 
@@ -23,24 +23,24 @@ public class Player {
     }
 
     public static void totalPlayers(int numb) {
-        playernumbers = numb;
-        players = new Player[playernumbers]; // Initialize the players array
-        System.out.println(playernumbers + " Number of Players!"); // This is a test, remove later
+        playerNumbers = numb;
+        players = new Player[playerNumbers]; // Initialize the players array
+        System.out.println(playerNumbers + " Number of Players!"); // This is a test, remove later
     }
 
     public static Player[] createPlayers() {
-        for (int i = 0; i < playernumbers; i++) {
+        for (int i = 0; i < playerNumbers; i++) {
             players[i] = new Player("Player " + (i + 1));
         }
         return players;
     }
 
-    public static Player[] getAllPlayers() {
-        return players;
+    public static Player getPlayerNumb(int number) {
+        return players[number];
     }
 
     public static void switchPlayer() {
-        playerIndex = (playerIndex + 1) % playernumbers;
+        playerIndex = (playerIndex + 1) % playerNumbers;
     }
 
     public static Player getCurrentPlayer() {
