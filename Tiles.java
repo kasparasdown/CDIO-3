1291a2f4bb207dbc19b2c86d52537770c21489f7
Victor Mazanti <victormazanti@hotmail.com>
2023-11-23 12:46:03 +0100

    prices for rent and purchase are now correct

--- Tiles.java
+++ Tiles.java
@@ -25,29 +25,29 @@ public class Tiles {
     public static void createTiles() {
         tiles = new Tiles[24];
         tiles[0] = new StartField(0,"Start");
-        tiles[1] = new PropertyField(1, "Burger Joint 1", 2, 3, null);
-        tiles[2] = new PropertyField(2, "Pizza House 2", 2, 3, null);
+        tiles[1] = new PropertyField(1, "Burger Joint 1", 1, 1, null);
+        tiles[2] = new PropertyField(2, "Pizza House 2", 1, 1, null);
         tiles[3] = new ChanceField(3, "Chancefield");//CHANGE THIS
-        tiles[4] = new PropertyField(4, "Candy Store 4", 2, 3, null);
-        tiles[5] = new PropertyField(5, "Ice Cream Parlour 5", 2, 3, null);
+        tiles[4] = new PropertyField(4, "Candy Store 4", 1, 1, null);
+        tiles[5] = new PropertyField(5, "Ice Cream Parlour 5", 1, 1, null);
         tiles[6] = new NoAction(6, "Jail visit 6");
-        tiles[7] = new PropertyField(7, "Museum 7", 2, 3, null);
-        tiles[8] = new PropertyField(8, "Library 8", 2, 3, null);
+        tiles[7] = new PropertyField(7, "Museum 7", 2, 2, null);
+        tiles[8] = new PropertyField(8, "Library 8", 2, 2, null);
         tiles[9] = new ChanceField(9, "Chancefield");
-        tiles[10] = new PropertyField(10, "Skate Park 10", 2, 3, null);
-        tiles[11] = new PropertyField(11, "Swimming Pool 11", 2, 3, null);
+        tiles[10] = new PropertyField(10, "Skate Park 10", 2, 2, null);
+        tiles[11] = new PropertyField(11, "Swimming Pool 11", 2, 2, null);
         tiles[12] = new NoAction(12, "Free Parking 12");
-        tiles[13] = new PropertyField(13, "Video Game Arcade 13", 2, 3, null);
-        tiles[14] = new PropertyField(14, "Movie Theater 14", 2, 3, null);
+        tiles[13] = new PropertyField(13, "Video Game Arcade 13", 3, 3, null);
+        tiles[14] = new PropertyField(14, "Movie Theater 14", 3, 3, null);
         tiles[15] = new ChanceField(15, "Chancefield");
-        tiles[16] = new PropertyField(16, "Toy Store 16", 2, 3, null);
-        tiles[17] = new PropertyField(17, "Pet Store 17", 2, 3, null);
+        tiles[16] = new PropertyField(16, "Toy Store 16", 3, 3, null);
+        tiles[17] = new PropertyField(17, "Pet Store 17", 3, 3, null);
         tiles[18] = new NoAction(18, "Jail visit 18"); //Change to go to prison later
-        tiles[19] = new PropertyField(19, "Bowling Alley 19", 2, 3, null);
-        tiles[20] = new PropertyField(20, "The Zoo 20", 2, 3, null);
+        tiles[19] = new PropertyField(19, "Bowling Alley 19", 4, 4, null);
+        tiles[20] = new PropertyField(20, "The Zoo 20", 4, 4, null);
         tiles[21] = new ChanceField(21, "Chancefield");
-        tiles[22] = new PropertyField(22, "Park Place 22", 2, 3, null);
-        tiles[23] = new PropertyField(23, "Broadwalk 23", 2, 3, null);
+        tiles[22] = new PropertyField(22, "Park Place 22", 5, 5, null);
+        tiles[23] = new PropertyField(23, "Broadwalk 23", 5, 5, null);
     }
     public static Tiles[] getAllTiles() {
         return tiles;
